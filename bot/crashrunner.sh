9228bccf521fee00881e040881610a4bb77be773
scriptcie-kathinka <scriptcie@professorfrancken.nl>
2015-06-07 23:57:14 +0200

    now appends the irc-logs.

--- bot/crashrunner.sh
+++ bot/crashrunner.sh
@@ -3,5 +3,6 @@ echo "THIS IS KATHINKA-BOT RUNNING!"
 # forever -c sh start crashrunner.sh
 cd ~/Kathinka/bot
 git pull origin master
-node irc_kathinka.js Kathinka-bot ***REMOVED***> kathinkalog.txt
+echo "NEW RUN OF KATHINKA-BOT IRC" >> kathinkalog.txt
+node irc_kathinka.js Kathinka-bot ***REMOVED*** >> kathinkalog.txt
 
