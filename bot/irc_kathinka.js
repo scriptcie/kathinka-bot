ef28c9e89bae93a655043acffb05e451b3ebb572
Mark Redeman <markredeman@gmail.com>
2015-06-07 12:31:16 +0200

    Consistentie in code style.
    In javascript is het over het algemeen best practice om ; tegebruiken en accolades niet op een nieuwe regel te zetten.

--- bot/irc_kathinka.js
+++ bot/irc_kathinka.js
@@ -19,7 +19,7 @@ config = {
         port: 6667
     },
     data: {}
-}
+};
 
 
 ////////////////////////////////////
@@ -27,21 +27,18 @@ config = {
 ////////////////////////////////////
 irc.socket = new net.Socket();
 // als er data binnenkomt
-irc.socket.on('data', function(data)
-              {
-                  // lees de data regel voor regel
-                  data = data.split('\n');
-                  for (var i = 0; i < data.length; i++)
-                  {
-                      // print wat er binnenkomt
-                      //console.log('RECV -', data[i]);
-                      // als het geen lege regel is haal dan even de extra enter weg. Dat doet IRC blijkbaar een \r\n situatie. mss hadden we ook kunnen splitten op \r\n?
-                      if (data !== '')
-                      {
-                          irc.handle(data[i].slice(0, -1));
-                      }
-                  }
-              });
+irc.socket.on('data', function(data) {
+    // lees de data regel voor regel
+    data = data.split('\n');
+    for (var i = 0; i < data.length; i++) {
+        // print wat er binnenkomt
+        //console.log('RECV -', data[i]);
+        // als het geen lege regel is haal dan even de extra enter weg. Dat doet IRC blijkbaar een \r\n situatie. mss hadden we ook kunnen splitten op \r\n?
+        if (data !== '') {
+            irc.handle(data[i].slice(0, -1));
+        }
+    }
+});
 
 
 irc.socket.on('connect', function() {
@@ -49,17 +46,16 @@ irc.socket.on('connect', function() {
     console.log('Established connection, registering and misc...');
     setTimeout(function(){  }, 2000);
     // bouw listener: als PING, dan PONG. irc.on staat hieronder.
-    irc.on(/^PING :(.+)$/i, function(info)
-            {
-                irc.raw('PONG :' + info[1]);
-            });
+    irc.on(/^PING :(.+)$/i, function(info) {
+        irc.raw('PONG :' + info[1]);
+    });
 
     // laat zien wanneer je connected bent
-    irc.on(/End of \/MOTD command/i, function(info){
+    irc.on(/End of \/MOTD command/i, function(info) {
         console.log("CONNECTED!");
         irc.raw("PRIVMSG NickServ :IDENTIFY " + config.user.pass);
         irc.raw("JOIN #script?cie");
-    })
+    });
 
     ////////////////////////////////////
     // CORE BUSINESS                  //
@@ -69,6 +65,8 @@ irc.socket.on('connect', function() {
         var channel = info[2];
         var data = info[3];
         var match;
+
+
         if (!(/^.*#.*$/.test(channel))) {
             irc.speak("PRIVMSG " + user + " :" + "* I AM KATHINKA-BOT *");
             return;
@@ -76,12 +74,15 @@ irc.socket.on('connect', function() {
 
         if  (match = data.match(/^[Kk]athinka(-bot)?(.*)$/)) {
             var actual_data = match[2];
+
+            // , AF
             if (/^[,:]{0,1} AF.*$/.test(actual_data)) {
                 irc.raw("QUIT");
                 process.exit(1);
                 return;
             }
 
+            // Random response
             if (/^.*\?$/.test(actual_data)) {
                 var eightball = [
                     "It is certain", "It is decidedly so", "Without a doubt",
@@ -95,11 +96,13 @@ irc.socket.on('connect', function() {
                 return;
             }
 
+            // ???
             if  (match = actual_data.match(/^[,:]{0,1} set (.*?) (.*)$/)) {
                 config.data[match[1]] = match[2];
                 return;
             }
 
+            // Agenda
             if (/^[,:]? ?agenda$/.test(actual_data)) {
                 if ('agenda' in config.data) {
                     irc.raw("PRIVMSG " + channel + " :" + config.data['agenda']);
@@ -108,10 +111,13 @@ irc.socket.on('connect', function() {
             }
         }
 
-        if (/^(le'?ah)|(sl[ea][ea]p)|(later)|((wel)?te?rusten?)$/.test(data)){
+        // Tell user goodbye
+        if (/^(le'?ah)|(sl[ea][ea]p)|(later)|((wel)?te?rusten?)$/.test(data)) {
             irc.speak("PRIVMSG " + channel + " :" + data + ", " + user);
             return;
         }
+
+        // I AM KATHINKA
         if (/^.*[Kk]athinka.*$/.test(data)) {
             irc.speak("PRIVMSG " + channel + " :" + "* I AM KATHINKA-BOT *");
             return;
@@ -131,50 +137,47 @@ irc.socket.on('connect', function() {
     ////////////////////////////////////
 
     // wacht heel even voor je de NICK informatie stuurt
-    setTimeout(function()
-                {
-                    irc.raw('NICK ' + config.user.nick);
-                    irc.raw('USER ' + config.user.user + ' 8 * :' + config.user.real);
-                }, 500);
+    setTimeout(function() {
+        irc.raw('NICK ' + config.user.nick);
+        irc.raw('USER ' + config.user.user + ' 8 * :' + config.user.real);
+    }, 500);
 });
 
 // settings + connect
 irc.socket.setEncoding('ascii');
 irc.socket.setNoDelay();
-setTimeout(function(){irc.socket.connect(config.server.port, config.server.addr);}, 2000);
+setTimeout(function() {
+    irc.socket.connect(config.server.port, config.server.addr);
+}, 2000);
 
 
 //handles incoming messages
-irc.handle = function(data)
-{
-    var i, info;
-    for (i = 0; i < irc.listeners.length; i++)
-    {
-        info = irc.listeners[i][0].exec(data);
-        if (info)
-        {
+irc.handle = function(data) {
+    for (var i = 0; i < irc.listeners.length; i++) {
+        var info = irc.listeners[i][0].exec(data);
+        if (info) {
             //console.log(info);
             irc.listeners[i][1](info, data);
         }
     }
 }
 
+// Event listening
 irc.listeners = [];
-
-irc.on = function(data, callback)
-{
+irc.on = function(data, callback) {
     irc.listeners.push([data, callback, false])
 }
-irc.speak = function(data){
-    setTimeout(function(){irc.raw(data);},data.length*5);
+
+// Wait a few seconds such that we get a realistic response
+irc.speak = function(data) {
+    return setTimeout(data.length*50, irc.raw(data));
 }
 
-irc.raw = function(data)
-{
-    irc.socket.write(data + '\n', 'ascii', function()
-                     {
-                         console.log('SENT :', data);
-                     });
+// Send the message to irc and log it
+irc.raw = function(data) {
+    irc.socket.write(data + '\n', 'ascii', function() {
+        console.log('SENT :', data);
+    });
 }
 ////////////////////////////////////
 // KATHINKA-BOT IRC               //
