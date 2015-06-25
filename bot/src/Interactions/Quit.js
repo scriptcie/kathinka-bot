310676dc5cc708ebd5bec5b0b138a33273746abe
Mark Redeman <markredeman@gmail.com>
2015-06-25 13:57:40 +0200

    Quit interactie quit correct

--- bot/src/Interactions/Quit.js
+++ bot/src/Interactions/Quit.js
@@ -1,13 +1,22 @@
-var Quit = function() {}
+var isACommand = require('../Helpers/IsACommand.js');
+var Quit = function(client) {
+    // Meh lelijk, maar werkt voorlopig
+    this.client = client;
+}
 
 Quit.prototype = {
     interact: function(message, from) {
+        var command = isACommand(message);
+        if (command && /^af!?$/.test(command)) {
+            this.client.disconnect("Is goed, doei!", function() {
+                    setTimeout(function() {
+                    process.exit(1);
+                }, 1000);
+            });
+            // return "QUIT";
+        }
 
-        setTimeout(function() {
-            process.exit(1);
-        }, 1000);
-
-        return "QUIT";
+        return undefined;
     },
 }
 
--- bot/test/Interactions/Quit_test.js
+++ bot/test/Interactions/Quit_test.js
@@ -1,20 +1,34 @@
 var Quit = require ('../../src/Interactions/Quit.js');
 var sinon = require('sinon');
 
-describe("Say my name interaction", function() {
-    var quit = new Quit;
+describe("Quit interaction", function() {
     var sender = "Mark";
 
-    it("Stops running", sinon.test(function() {
+    it("Stops running when asked to stop", sinon.test(function() {
+        var client = { disconnect: function(message, callback) {
+            callback();
+        }};
+
+        var quit = new Quit(client);
         var clock = sinon.useFakeTimers();
 
-        var subbedProcess = this.stub(process, 'exit').returns(0);
-        var response = quit.interact("Kathinka af!", sender);
-        response.should.equal("QUIT");
+        // We want to check if Kathinka exits herself.
+        var subbedProcess = this.stub(process, 'exit').returns(undefined);
 
+        var response = quit.interact("Kathinka af!", sender);
+        (response === undefined).should.be.true;
+
+        // The process should quit after some seconds such that we have time to tell
+        // irc that we want to disconnect
         subbedProcess.calledOnce.should.be.false;
         clock.tick(1000);
         subbedProcess.calledOnce.should.be.true;
         clock.restore();
     }));
+
+    it("Does not respond when not asked to stop", function() {
+        var quit = new Quit;
+        var response = quit.interact("Een bericht", sender);
+        (response === undefined).should.be.true;
+    });
 });
