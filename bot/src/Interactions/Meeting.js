492af3b76c66a9ae61dbd048fa1d6307d247524b
Sven (Sbte) <svenb.linux@gmail.com>
2015-07-24 21:50:36 +0200

    Add stub Meeting

---
+++ bot/src/Interactions/Meeting.js
@@ -0,0 +1,25 @@
+var isACommand = require('../Helpers/IsACommand.js');
+
+var Meeting = function(data) {
+    this.data = data;
+}
+
+Meeting.prototype = {
+    interact: function(message, from) {
+        var command = isACommand(message);
+        if (command !== null) {
+            var response = this.handleCommand(command, from);
+            if (response !== undefined) {
+                return response;
+            }
+        }
+
+        return undefined;
+    },
+
+    handleCommand: function(command, sender) {
+        return undefined;
+    },
+}
+
+module.exports = Meeting
