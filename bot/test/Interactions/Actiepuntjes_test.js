78be5abf67dee55e7fafc5b48b1d6f4bb426603f
Mark Redeman <markredeman@gmail.com>
2015-07-24 22:01:24 +0200

    Assert that responses of AP interactions should be deeply equal instead of strictly equal

--- bot/test/Interactions/Actiepuntjes_test.js
+++ bot/test/Interactions/Actiepuntjes_test.js
@@ -6,21 +6,21 @@ describe("The Actiepuntjes interaction", function() {
 
     it("Saves new actiepuntjes", function() {
         var response = AP.interact("AP plus fix actiepuntjes", "mark");
-        AP.data.should.equal({"plus": ["fix actiepuntjes"]});
+        AP.data.should.eql({"plus": ["fix actiepuntjes"]});
     });
 
     it("ignores AP anywhere else than at the start of a sentence",function(){
         AP.interact("hoi hoi AP hoi hoi", "mark");
-        AP.data.should.equal({"plus": ["fix actiepuntjes"]});       
+        AP.data.should.eql({"plus": ["fix actiepuntjes"]});
     });
 
     it("Displays all actiepuntjes", function() {
     	var response = AP.interact("AP", "mark");
-        response.should.equal(["AP plus fix actiepuntjes"]);
+        response.should.eql(["AP plus fix actiepuntjes"]);
     });
 
     it("Displays one person's actiepuntjes", function() {
     	var response = AP.interact("AP plus", "mark[1]");
-    	response.should.equal(["AP plus fix actiepuntjes"]);
+    	response.should.eql(["AP plus fix actiepuntjes"]);
     });
 });
