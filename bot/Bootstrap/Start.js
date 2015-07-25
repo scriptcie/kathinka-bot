44b78a820f69d610818eeb0ed4719d48e31008d0
Mark Redeman <markredeman@gmail.com>
2015-07-25 14:40:59 +0200

    Use a factory function to create a kathinka instance

--- bot/Bootstrap/Start.js
+++ bot/Bootstrap/Start.js
@@ -1,47 +1,16 @@
 #!/usr/bin/env node
-
-// Build kathinka bot with some interactions
-function buildKathinka(client) {
-    var Kathinka = require('../src/Kathinka.js');
-    var Eightball = require('../src/Interactions/Eightball.js');
-    var Goodbye = require('../src/Interactions/Goodbye.js');
-    var Logging = require('../src/Interactions/Logging.js');
-    var SayMyName = require('../src/Interactions/SayMyName.js');
-    var Properties = require('../src/Interactions/Properties.js');
-    var Quit = require('../src/Interactions/Quit.js');
-    var Actiepuntjes = require('../src/Interactions/Actiepuntjes.js');
-
-    var askForAdvice = require('../src/Helpers/IsAQuestion.js');
-
-    var dataStore = {
-        logging: {},
-        properties: {},
-    };
-
-    var kathinka = new Kathinka([
-        new SayMyName,
-        new Goodbye,
-        new Eightball(askForAdvice),
-        new Logging(dataStore.logging),
-        new Properties(dataStore.properties),
-        new Quit(client),
-        new Actiepuntjes,
-    ]);
-
-    return kathinka;
-}
+var buildKathinka = require('../src/Helpers/KathinkaFactory');
+var BotNet = require('../src/BotNet.js');
+var irc = require('irc');
 
 var username = process.argv[2] || "Kathinka-Bot-test";
 var password = process.argv[3] || "";
 
-var irc = require('irc');
 var client = new irc.Client('irc.freenode.net', username, {
     debug: true,
     channels: ['#script?cie'],
     password: password,
 });
-
-var BotNet = require('../src/BotNet.js');
 
 var kathinka = buildKathinka(client);
 var botNet = new BotNet(client, [kathinka]);
