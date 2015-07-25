5ec96e5bcc9097daf6c73cb2afa92e14f0521291
Sven (Sbte) <svenb.linux@gmail.com>
2015-07-25 13:08:52 +0200

    Add a stop meeting test

--- bot/test/Interactions/Meeting_test.js
+++ bot/test/Interactions/Meeting_test.js
@@ -12,12 +12,24 @@ describe("Meeting interaction", function() {
 
     it("Starts a meeting when the agenda is set", function() {
         var meeting = new Meeting({'agenda': 'test'});
+        meeting.started.should.be.false;
+
         var response = meeting.interact("Kathinka, start meeting", sender);
-
         (response === undefined).should.be.false;
-
         response.should.equal("Starting meeting");
-
         meeting.started.should.be.true;
+    });
+
+    it("Stops a meeting when one is started", function() {
+        var meeting = new Meeting({'agenda': 'test'});
+        meeting.started.should.be.false;
+
+        var response = meeting.interact("Kathinka, start meeting", sender);
+        meeting.started.should.be.true;
+
+        response = meeting.interact("Kathinka, stop meeting", sender);
+        (response === undefined).should.be.false;
+        response.should.equal("Stopping meeting");
+        meeting.started.should.be.false;
     });
 });
