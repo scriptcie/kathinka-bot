cb9c271b5d80f72ad7e4b7d19e73f2ef0559132d
Plus <plus@vient.nl>
2015-08-29 22:52:44 +0200

    Previous version tried responding to Telegram message objects that had no message.text property. Kathinka subsequently tried to panic a lot. This did not work. Then she faints. Now this is fixed by checking if the message actually contains a message.text property.

--- bot/src/Telegram.js
+++ bot/src/Telegram.js
@@ -13,12 +13,14 @@ var Telegram = function(config, bots) {
     var self = this;
 
     this.api.on('message', function(message){
-        self.bot[0].notify(message.text,message.from.first_name, function(messages) {
-                for (var m = 0; m < messages.length; m++) {
-		    console.log(messages[m]);
-                    self.api.sendMessage({ chat_id: message.chat.id, text: messages[m]}, function(err, data) {console.log(err); console.log(util.inspect(data, false, null));});
-                };
-            });
+	if(message.text){
+	        self.bot[0].notify(message.text || "", message.from.first_name, function(messages) {
+	                for (var m = 0; m < messages.length; m++) {
+			    console.log(messages[m]);
+	                    self.api.sendMessage({ chat_id: message.chat.id, text: messages[m]}, function(err, data) {console.log(err); console.log(util.inspect(data, false, null));});
+	                };
+	            });
+		}
         return;
     });
 };
