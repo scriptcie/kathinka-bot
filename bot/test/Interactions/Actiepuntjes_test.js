ea0eecd471039a73d40fb71504efd4596d53cc39
Sven (Sbte) <svenb.linux@gmail.com>
2015-08-30 10:53:45 +0200

    Get actiepuntjes data from state.actiepuntjes, instead of state
    Fixes #17

--- bot/test/Interactions/Actiepuntjes_test.js
+++ bot/test/Interactions/Actiepuntjes_test.js
@@ -3,13 +3,13 @@ var Actiepuntjes = require ('../../src/Interactions/Actiepuntjes.js');
 describe("The Actiepuntjes interaction", function() {
 
     it("Saves new actiepuntjes", function() {
-        var AP = new Actiepuntjes({});
+        var AP = new Actiepuntjes({actiepuntjes: {}});
         var response = AP.interact("kathinka AP plus fix actiepuntjes", "mark");
         AP.data.should.eql({"plus": ["fix actiepuntjes"]});
     });
 
     it("Displays all actiepuntjes", function() {
-        var AP = new Actiepuntjes({});
+        var AP = new Actiepuntjes({actiepuntjes: {}});
         AP.interact("kathinka AP plus fix actiepuntjes", "mark");
         var response = AP.interact("kathinka AP ls", "mark");
 
@@ -19,7 +19,7 @@ describe("The Actiepuntjes interaction", function() {
 
     it("Removes a person actiepuntje", function() {
         ["rm", "voltooid", "gedaan"].forEach(function(rm) {
-            var AP = new Actiepuntjes({"plus": ["fix actiepuntjes"]});
+            var AP = new Actiepuntjes({actiepuntjes: {"plus": ["fix actiepuntjes"]}});
             var message = "kathinka AP " + rm + " fix actiepuntjes";
             var response = AP.interact(message, "mark[1]");
             response.should.equal("Goed bezig plus");
@@ -28,7 +28,7 @@ describe("The Actiepuntjes interaction", function() {
     });
 
     it("Removes a person actiepuntje given an index", function() {
-        var AP = new Actiepuntjes({});
+        var AP = new Actiepuntjes({actiepuntjes: {}});
         AP.data = {"plus": ["ap1", "ap2"], "mark": ["ap3", "ap4"]};
         AP.interact("kathinka AP rm 3", "mark[1]");
         AP.data.should.eql({"plus": ["ap1", "ap2"], "mark": ["ap4"]});
