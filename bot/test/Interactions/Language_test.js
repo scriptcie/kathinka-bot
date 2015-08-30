932b0c3cc491069ce896ca2bba02ef5ffaaf2d87
Sven (Sbte) <svenb.linux@gmail.com>
2015-08-31 00:13:39 +0200

    Port Goodbye and Language to the Message system

--- bot/test/Interactions/Language_test.js
+++ bot/test/Interactions/Language_test.js
@@ -1,4 +1,5 @@
 var Language = require ('../../src/Interactions/Language.js');
+var Message = require ('../../src/Message.js');
 
 function someDetector() {
     // a helper for if we want to provide a log repository for the logger
@@ -10,27 +11,34 @@ describe("The language detection interaction", function() {
 
     it("Can detect Dutch", function() {
         var detector = someDetector();
-        detector.interact("hoi numwis", sender);
+        var message = new Message(Message.Type.Null, "hoi numwis", sender);
+        detector.interact(message, sender);
         detector.state.language.should.equal('dutch');
     });
 
     it("Can detect Dutch", function() {
         var detector = someDetector();
-        detector.interact("hoi numwis", sender);
+        var message = new Message(Message.Type.Null, "hoi numwis", sender);
+        detector.interact(message, sender);
         detector.state.language.should.equal('dutch');
-        detector.interact("hoe gaat het?", sender);
+        message = new Message(Message.Type.Null, "hoe gaat het?", sender);
+        detector.interact(message, sender);
         detector.state.language.should.equal('dutch');
-        detector.interact("ik snap de properties class niet", sender);
+        message = new Message(Message.Type.Null, "ik snap de properties class niet", sender);
+        detector.interact(message, sender);
         detector.state.language.should.equal('dutch');
     });
 
     it("Can detect English", function() {
         var detector = someDetector();
-        detector.interact("hey numwis", sender);
+        var message = new Message(Message.Type.Null, "hey numwis", sender);
+        detector.interact(message, sender);
         detector.state.language.should.equal('english');
-        detector.interact("how are you?", sender);
+        message = new Message(Message.Type.Null, "how are you?", sender);
+        detector.interact(message, sender);
         detector.state.language.should.equal('english');
-        detector.interact("I don't understand the properties class", sender);
+        message = new Message(Message.Type.Null, "I don't understand the properties class", sender);
+        detector.interact(message, sender);
         detector.state.language.should.equal('english');
     });
 });
