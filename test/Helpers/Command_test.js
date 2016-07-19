4173a3864b1b891b58a8cf4bfe83b11eea1db86f
Sven (Sbte) <svenb.linux@gmail.com>
2016-07-19 20:20:44 +0200

    Add a Command class to be able to better handle commands

---
+++ test/Helpers/Command_test.js
@@ -0,0 +1,97 @@
+var Message = require ('../../src/Message.js');
+var Command = require ('../../src/Helpers/Command.js');
+
+describe("Command", function() {
+    var prefixes = [
+        "Kathinka-bot",
+        "kathinka-bot",
+        "Kathinka",
+        "kathinka",
+
+        "Kathinka-bot, ",
+        "kathinka-bot, ",
+        "Kathinka, ",
+        "kathinka, ",
+
+        "Kathinka-bot: ",
+        "kathinka-bot: ",
+        "Kathinka: ",
+        "kathinka: ",
+    ];
+
+    it("can handle Message objects", function() {
+        prefixes.forEach(function(prefix) {
+            var message = new Message(Message.Type.Null,
+                                      prefix + " work",
+                                      "Mark");
+            var command = new Command(/work$/, '', message, function() {
+                return "works";
+            });
+            var response = command.handle();
+            (response === undefined).should.be.false;
+            response.should.equal("works");
+        });
+    });
+
+    it("can handle strings", function() {
+        prefixes.forEach(function(prefix) {
+            var message = "work";
+            var command = new Command(/work$/, '', message, function() {
+                return "works";
+            });
+            var response = command.handle();
+            (response === undefined).should.be.false;
+            response.should.equal("works");
+        });
+    });
+
+    it("can handle subcommands", function() {
+        prefixes.forEach(function(prefix) {
+            var message = "work now";
+            var command = new Command(/work/, '', message, function() {
+                return "doesn't work";
+            });
+            command.add(new Command(/now/, '', message, function() {
+                return "works";
+            }));
+            var response = command.handle();
+            (response === undefined).should.be.false;
+            response.should.equal("works");
+        });
+    });
+
+    it("can handle subcommands with extra matching groups", function() {
+        prefixes.forEach(function(prefix) {
+            var message = "work now";
+            var command = new Command(/(work)/, '', message, function() {
+                return "doesn't work";
+            });
+            command.add(new Command(/now/, '', message, function() {
+                return "works";
+            }));
+            var response = command.handle();
+            (response === undefined).should.be.false;
+            response.should.equal("works");
+        });
+    });
+});
+
+describe("CommandList", function() {
+    var prefix = "Kathinka-bot";
+
+    it("can handle multiple Commands", function() {
+        var message = new Message(Message.Type.Null,
+                                  prefix + " work",
+                                  "Mark");
+        var commandList = new Command.List();
+        commandList.add(new Command(/work$/, '', message, function() {
+            return "works";
+        }));
+        commandList.add(new Command(/chill$/, '', message, function() {
+            return "doesn't work";
+        }));
+        var response = commandList.handle();
+        (response === undefined).should.be.false;
+        response.should.equal("works");
+    });
+});
