b69141d630c4160130b6d16cf8ed76c1336e4327
Sven (Sbte) <svenb.linux@gmail.com>
2016-07-19 20:56:09 +0200

    Use Command in Meeting

--- src/Interactions/Meeting.js
+++ src/Interactions/Meeting.js
@@ -1,4 +1,5 @@
 var Message = require('../Message.js');
+var Command = require('./../Helpers/Command.js');
 
 var Meeting = function(state, bus) {
     this.state = state
@@ -30,32 +31,49 @@ Meeting.prototype = {
         return response;
     },
 
-    handleCommand: function(command, sender, protocol) {
-        if ('agenda' in this.data &&
-            (command === "start meeting" || command === "start vergadering")) {
-            this.started = true;
-            var response = ['Staring meeting', 'Agenda:'];
-            this.setAgenda();
-            this.index = 0;
-            this.protocol = protocol;
-            response.push.apply(response, this.agenda);
-            return response;
-       }
+    handleCommand: function(message, sender, protocol) {
+        var commandList = new Command.List();
+        if ('agenda' in this.data) {
+            commandList.add(
+                new Command(
+                    ['start meeting', 'start vergadering'],
+                    'Start a meeting if agenda is set using: set agenda [digitale anarchie, vodka]',
+                    message, function() {
+                        this.started = true;
+                        var response = ['Staring meeting', 'Agenda:'];
+                        this.setAgenda();
+                        this.index = 0;
+                        this.protocol = protocol;
+                        response.push.apply(response, this.agenda);
+                        return response;
+                    }.bind(this)));
 
-        if (this.started && command === "next") {
-            return this.goNext(true);
+            commandList.add(
+                new Command(
+                    'agenda', 'Get the current agenda',
+                    message, function() {
+                        this.setAgenda();
+                        return this.agenda;
+                    }.bind(this)));
         }
 
-        if (command === "stop meeting" || command === "stop vergadering") {
-            this.started = false;
-            return "End of the meeting";
+        if (this.started) {
+            commandList.add(
+                new Command('next', 'Go to the next item on the agenda',
+                            message, function() {
+                                return this.goNext(true);
+                            }.bind(this)));
+
+            commandList.add(
+                new Command(['stop meeting', 'stop vergadering'],
+                            'Go to the next item on the agenda',
+                            message, function() {
+                                this.started = false;
+                                return "End of the meeting";
+                            }.bind(this)));
         }
 
-        if ('agenda' in this.data && command === "agenda") {
-            this.setAgenda();
-            return this.agenda;
-        }
-        return undefined;
+        return commandList.handle();
     },
 
     setAgenda: function() {
