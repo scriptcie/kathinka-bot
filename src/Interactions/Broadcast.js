6bb71a2689a05e691b6a406bb35c62e817268ea1
Sven (Sbte) <svenb.linux@gmail.com>
2016-07-19 12:06:31 +0200

    Only broadcast to channels and Steam
    Fixes #7

--- src/Interactions/Broadcast.js
+++ src/Interactions/Broadcast.js
@@ -14,11 +14,20 @@ Broadcast.prototype = {
     interact: function(message, from) {
         message = Message.fromMessage(message, from);
 
-        // Add channels to state.channels so we can spam them back
-        if (!(message.type in this.data)) {
-            this.data[message.type] = [message.to];
-        } else if (this.data[message.type].indexOf(message.to) === -1) {
-            this.data[message.type].push(message.to);
+        // Add channels to state.channels so we can spam them back.
+        // At the moment we spam public channels and everyone on Steam
+        if (!message.private || message.type === Message.Type.Steam) {
+            if (!(message.type in this.data)) {
+                this.data[message.type] = [message.to];
+            } else if (this.data[message.type].indexOf(message.to) === -1) {
+                this.data[message.type].push(message.to);
+            }
+        } else if (message.type in this.data) {
+            // Remove private channels if they were already in there
+            var index = this.data[message.type].indexOf(5);
+            if (index > -1) {
+                this.data[message.type].splice(index, 1);
+            }
         }
 
         var command = message.command();
--- test/Interactions/Broadcast_test.js
+++ test/Interactions/Broadcast_test.js
@@ -5,13 +5,14 @@ var MessageBus = require ('../../src/MessageBus.js');
 describe("The Broadcast interaction", function() {
     var sender = "Mark";
 
-    it("Sends a broadcast message to the bus", function() {
+    it("Sends a broadcast message to the bus", function(done) {
         var bus = new MessageBus({});
         var stubbedInterface = {say: function(to, messages) {
             (messages === undefined).should.be.false;
             (messages[0] === undefined).should.be.false;
             messages[0].should.eql("plus fix actiepuntjes");
             to.should.eql(sender);
+            done();
         }};
         bus.addInterface(Message.Type.Null, stubbedInterface);
 
@@ -21,7 +22,7 @@ describe("The Broadcast interaction", function() {
         response.should.equal("Message broadcasted");
     });
 
-    it("Sends a broadcast message to the bus", function() {
+    it("Sends a broadcast message to the right channels", function(done) {
         var bus = new MessageBus({});
         var messageTypes = [Message.Type.Null, Message.Type.IRC, Message.Type.Steam];
         var checked = 0;
@@ -31,8 +32,12 @@ describe("The Broadcast interaction", function() {
                 (messages[0] === undefined).should.be.false;
                 messages[0].should.eql("plus fix actiepuntjes");
                 to.should.eql(sender + messageType);
+                messageType.should.not.eql(Message.Type.Null);
                 checked++;
                 checked.should.be.below(3);
+                if(checked == 2) {
+                    done();
+                }
             }};
             bus.addInterface(messageType, stubbedInterface);
         });
@@ -47,6 +52,77 @@ describe("The Broadcast interaction", function() {
 
         // Test if it sends back to both of the above channels
         var message = new Message(Message.Type.IRC, "Kathinka broadcast plus fix actiepuntjes", sender + Message.Type.IRC);
+        var response = broadcast.interact(message, sender);
+        (response === undefined).should.be.false;
+        response.should.equal("Message broadcasted");
+    });
+
+    it("Doesn't broadcast to private messages", function(done) {
+        var bus = new MessageBus({});
+        var messageTypes = [Message.Type.Null, Message.Type.IRC, Message.Type.Steam];
+        var checked = 0;
+        messageTypes.forEach(function(messageType) {
+            var stubbedInterface = {say: function(to, messages) {
+                (messages === undefined).should.be.false;
+                (messages[0] === undefined).should.be.false;
+                messages[0].should.eql("plus fix actiepuntjes");
+                to.should.eql(sender + messageType);
+                messageType.should.not.eql(Message.Type.Null);
+                checked++;
+                checked.should.be.below(2);
+                done();
+            }};
+            bus.addInterface(messageType, stubbedInterface);
+        });
+
+        var broadcast = new Broadcast({}, bus);
+
+        // Feed some initial messages
+        var message = new Message(Message.Type.IRC, "Hallo", sender);
+        message.private = true;
+        broadcast.interact(message, sender);
+        var message = new Message(Message.Type.IRC, "Hallo", sender + Message.Type.IRC);
+        broadcast.interact(message, sender);
+
+        // Test if it sends back to both of the above channels
+        var message = new Message(Message.Type.IRC, "Kathinka broadcast plus fix actiepuntjes", sender);
+        message.private = true;
+        var response = broadcast.interact(message, sender);
+        (response === undefined).should.be.false;
+        response.should.equal("Message broadcasted");
+    });
+
+    it("Does broadcast to Steam", function(done) {
+        var bus = new MessageBus({});
+        var messageTypes = [Message.Type.Null, Message.Type.IRC, Message.Type.Steam];
+        var checked = 0;
+        messageTypes.forEach(function(messageType) {
+            var stubbedInterface = {say: function(to, messages) {
+                (messages === undefined).should.be.false;
+                (messages[0] === undefined).should.be.false;
+                messages[0].should.eql("plus fix actiepuntjes");
+                to.should.eql(sender + messageType);
+                messageType.should.not.eql(Message.Type.Null);
+                checked++;
+                checked.should.be.below(2);
+                done();
+            }};
+            bus.addInterface(messageType, stubbedInterface);
+        });
+
+        var broadcast = new Broadcast({}, bus);
+
+        // Feed some initial messages
+        var message = new Message(Message.Type.IRC, "Hallo", sender);
+        message.private = true;
+        broadcast.interact(message, sender);
+        var message = new Message(Message.Type.Steam, "Hallo", sender + Message.Type.Steam);
+        message.private = true;
+        broadcast.interact(message, sender);
+
+        // Test if it sends back to both of the above channels
+        var message = new Message(Message.Type.IRC, "Kathinka broadcast plus fix actiepuntjes", sender);
+        message.private = true;
         var response = broadcast.interact(message, sender);
         (response === undefined).should.be.false;
         response.should.equal("Message broadcasted");
