de501df9906b4dafa8f1e208ab9a488cea7c9ed1
Sven (Sbte) <svenb.linux@gmail.com>
2016-07-22 17:49:27 +0200

    Wait for the Github API to return for the tests to complete
    Should fix the remaining test failures

--- test/Helpers/Github_test.js
+++ test/Helpers/Github_test.js
@@ -4,20 +4,22 @@ var API = require('./../../src/Helpers/Github');
 describe("Github API", function() {
 
 
-    it("It gets a list of issues from github and it converts it to an array of issues", function() {
+    it("It gets a list of issues from github and it converts it to an array of issues", function(done) {
         var api = new API;
 
         api.issues(function(issues) {
             issues.should.be.instanceof(Array);
-                    });
+            done();
+        });
     });
 
-    it("It gets a list of issues from github assigned by username", function() {
+    it("It gets a list of issues from github assigned by username", function(done) {
         var api = new API;
 
         api.issuesAssignedFor("MarkRedeman", function(issues) {
             issues.should.be.instanceof(Array);
             issues[0].assignee.login.should.be.equal("MarkRedeman");
+            done();
         });
     });
 
